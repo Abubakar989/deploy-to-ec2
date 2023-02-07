9284f62051ca68fd652a0b715ccf1720dc1d83bc
Abubakar989 <absadiq989@gmail.com>
2023-02-07 13:15:20 +0500

    updatepot

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
 
-const PORT = process.env.PORT || 3002
+const PORT = process.env.PORT || 8080
 
 http.listen(PORT, () => {
     console.log(`update Listening on port ${PORT}`)
